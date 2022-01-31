f9689fc212132347fb810e7a0321ab2fc8ea750a
bhavyaaaaa29 <you@example.com>
2022-01-31 14:48:51 +0530

    practical 2 done

--- fourteen.java
+++ fourteen.java
@@ -1,6 +1,10 @@
 public class fourteen {
     public static void main(String[] args) {
-        
+        circle c1 = new circle();
+        circle c2 = new circle(10);
+        circle c3 = new circle();
+        circle c4 = new circle(1.25f);
+        c2.getNum();
     }
 }
 class circle{
@@ -12,15 +16,19 @@ class circle{
     }
     public circle(float r)
     {
-        this();
+        //this();
         setRadius(r);
     }
-    public void setRadius()
+    public void setRadius(float r)
     {
         this.radius = r;
     }
     public float getRadius()
     {
         return radius;
+    }
+    public void getNum()
+    {
+        System.out.println("The default constructor is called "+count+" times.");
     }
 }
