707413e345d1c1b02bda29d3ab4320a73469170c
bhavyaaaaa29 <you@example.com>
2022-01-31 09:06:58 +0530

    partial code done

---
+++ fourteen.java
@@ -0,0 +1,26 @@
+public class fourteen {
+    public static void main(String[] args) {
+        
+    }
+}
+class circle{
+    private float radius;
+    static int count= 0;
+    public circle()
+    {
+        count++;
+    }
+    public circle(float r)
+    {
+        this();
+        setRadius(r);
+    }
+    public void setRadius()
+    {
+        this.radius = r;
+    }
+    public float getRadius()
+    {
+        return radius;
+    }
+}
