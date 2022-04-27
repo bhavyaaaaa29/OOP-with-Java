601b58af02438fa1070c0cd7aca21670468d3c89
bhavyaaaaa29 <you@example.com>
2022-04-27 18:02:21 +0530

    done

--- twentyFour.java
+++ twentyFour.java
@@ -1,5 +1,45 @@
+
+
 public class twentyFour {
     public static void main(String[] args) {
-        
+        int a[]=new int[5];
+        try{        
+            a[5]=30/0;     
+           }    
+           catch(ArithmeticException e)  
+              {  
+               System.out.println("Arithmetic Exception occurs");  
+              }    
+              try
+              {
+                System.out.println(a[10]);  
+              }
+           catch(ArrayIndexOutOfBoundsException e)  
+              {  
+               System.out.println("ArrayIndexOutOfBounds Exception occurs");  
+              } 
+              try
+             {
+                classA();
+             }
+             catch(ArrayIndexOutOfBoundsException e)  
+              {  
+               System.out.println("ArrayIndexOutOfBounds Exception occurs");  
+              }
+           catch(Exception e)  
+              {  
+               System.out.println("Parent Exception occurs");  
+              }       
+              finally
+              {
+                  System.out.println("Inside finally block.");
+              }      
+           System.out.println("rest of the code");    
+    }  
+    public static void classA() throws ArrayIndexOutOfBoundsException
+    {
+        throw new ArrayIndexOutOfBoundsException("Index out of bounds.");
     }
+        
 }
+
--- twentyThree.java
+++ twentyThree.java
@@ -1,17 +1,15 @@
 
-import java.util.Scanner;
 
 public class twentyThree {
     public static void main(String[] args) {
-        circle c = new circle();
-        c.area();
+        new circle().findArea();
     }
 }
 final class circle
 {
     final float radius=10f;
     final float pi=3.14f;
-    public final void area()
+    final void findArea()
     {
         System.out.println("Area of the circle is "+(pi*radius*radius));
     }
